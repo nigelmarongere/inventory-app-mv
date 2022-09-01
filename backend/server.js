011916bb083e8200c7cfbc72f5b515e3ef58401b
Clint Shaw <clintshaw@Clints-MacBook-Pro.local>
2022-09-01 09:25:43 +0100

    requests added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,20 @@ const app = express()
 const seed = require('./seedData/seed')
 const {itemRoutes} = require('./routes');
 
+app.get('/', async (req, res) => {
+  const items = await Item.findAll();
+  res.send(items)
+})
+
+app.get('/:id', async (req, res) => {
+  const items = await Item.findByPk(req.params.id);
+  if(!items) {
+      res.status(404);
+    
+    }
+    res.send(items);
+})
+
 const server = async (port) => {
     await seed() 
     app.listen(port, () => {
